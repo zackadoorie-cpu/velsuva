23172d4eaa2e2624f88dbfa9e2a1aed12c6b61da
zackadoorie-cpu <zackadoorie@gmail.com>
2025-09-20 12:06:04 -0400

    Add files via upload

---
+++ velsuva-console/src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ControlPanelApp from './ControlPanelApp'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ControlPanelApp />
+  </React.StrictMode>
+)
